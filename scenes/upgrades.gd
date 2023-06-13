2663a0f6e811c117bd67cf5898fec043aa5811e1
Aloísio M. Lingo Filho <alingofilho@gmail.com>
2023-06-13 09:42:42 -0300

    teste de upgrades

--- scenes/upgrades.gd
+++ scenes/upgrades.gd
@@ -1,10 +1,14 @@
 extends Node
 
-var list = {
-	"name": "Campanha"
-}
+var list = [{
+	"name": "Campanha",
+	"img": "res://assets/sprites/campanha.jpg"}
+, 	{"name": "CU",
+	"img": "res://assets/sprites/button.png"
+}]
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Sprite2D.texture = load(list[1].img)
 	pass # Replace with function body.
 
 
