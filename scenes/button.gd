39e0f318c822e4bc72197dd37f6879f08f6d17c6
Aloísio M. Lingo Filho <alingofilho@gmail.com>
2023-06-12 22:25:33 -0300

    Coisitas

--- scenes/button.gd
+++ scenes/button.gd
@@ -1,23 +1,14 @@
 extends Area2D
+signal clicked()
 
-@onready var animation = $ButtonSprite/AnimationPlayer
-
-var blood = 0
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$ButtonSprite.frame = 0
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_released("click"):
 		$ButtonSprite.frame = 0
-	pass
 	
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		blood+=1
-		print(blood)
 		$ButtonSprite.frame = 1
-		
-
+		emit_signal("clicked")
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,11 +1,9 @@
 extends Node2D
 
+@onready var button = $Button
+@onready var bloodText = $BloodText
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _on_clicked():
+	Variables.blood += 1
+	bloodText.text = str(Variables.blood)
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
